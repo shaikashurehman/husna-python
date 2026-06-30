d2fda9c62ee1e48483687880b73c2a6600684211
shaikashurehman <shaikashurehman@gmail.com>
2026-06-30 14:55:23 +0300

    trignometery value.py

---
+++ trignometery value.py
@@ -0,0 +1,7 @@
+import math
+angle=float(input("enter angle in degrees:"))
+#convert degrees to radians
+radian=math.radians(angle)
+print("sin=",math.sin(radian))
+print("cos=",math.cos(radian))
+print("tan=",math.tan(radian))
