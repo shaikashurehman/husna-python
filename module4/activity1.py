6163a550d19266b1315eb7e106c2c5e9894e80c4
shaikashurehman <shaikashurehman@gmail.com>
2026-07-01 16:47:24 +0300

    lesson1

---
+++ module4/activity1.py
@@ -0,0 +1,9 @@
+empty_list=[]
+print(empty_list)
+number=[1,2,3,4,5]
+print(number)
+triples=[1,2,3]*3
+print("triple list",triples)
+aList=[100,200,300,400,500]
+reverse=aList[::-1]
+print(reverse)
---
+++ module4/activity2.py
@@ -0,0 +1,7 @@
+L = [4, 5, 1, 2, 9, 7, 10, 8]
+print("Original List :", L)
+print(len(L))
+L.sort()
+print(L)
+print("smallest element",L[0])
+print("largest element",L[-1])
