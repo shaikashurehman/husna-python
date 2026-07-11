1f56151553255db71236be0e521fd6d23f4b9e80
shaikashurehman <shaikashurehman@gmail.com>
2026-07-11 16:47:55 +0300

    lesson3

---
+++ module4/lesson3/activity1.py
@@ -0,0 +1,21 @@
+#1.Make a dictionary countrry code with country name as key and country code as value.
+
+#country_code = {'India' : '0091',
+
+# 'Australia' : '0025',
+
+# 'Nepal' : '00977'}
+
+#2. Search for the country code of India and print it.
+
+#print("Country code for India -")
+
+#print(country_code.gget('India', 'Not Found'))
+
+#3. Search for the country code of Japan and print it.
+country_code={'india':'95','south_korea':'81','kuwait':'76'}
+print("country code for india",country_code.get('india','not found'))
+print("country code for japan",country_code.get('japan','not found'))
+
+
+
---
+++ module4/lesson3/activity2.py
@@ -0,0 +1,11 @@
+husna_dic={'hindi':80,'english':90,'math':80,'science':80}
+print("husna dic",husna_dic)
+k=80
+result=0
+for key in husna_dic:
+    if husna_dic[key]==k:
+        result=result+1
+print("husna got 80 marks in",result)
+
+
+
