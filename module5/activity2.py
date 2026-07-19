8fa050fbfb3be7b7a3fcbb02eab68e5614008d02
shaikashurehman <shaikashurehman@gmail.com>
2026-07-19 16:48:13 +0300

    lesson1

---
+++ module5/activity2.py
@@ -0,0 +1,9 @@
+class Vehicle:
+  def __init__(self,max_speed,mileage):
+    self.max_speed=max_speed
+    self.mileage=mileage
+
+modelX=Vehicle(240,18)
+print("model max speed:",modelX.max_speed)
+print("model mileage:",modelX.mileage)
+
